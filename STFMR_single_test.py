433d6a874f1a71de1eb08c273316ccaba799354f
Akiyo Iwashina <akiyo@keio.jp>
2022-06-13 16:17:27 +0900

    STFMR_code update

--- STFMR_single_test.py
+++ STFMR_single_test.py
@@ -1,8 +1,8 @@
 #%%　imoprt package
 import pyvisa as visa
-import time  
-import matplotlib.pyplot as plt 
-import pandas as pd 
+import time
+import matplotlib.pyplot as plt
+#import pandas as pd
 import matplotlib.cm as cm
 import numpy as np
 
@@ -14,12 +14,12 @@ print(rm.list_resources())
 
 
 #%% define measurement equipment
-AgilentN5183A = rm.open_resource(  'GPIB0::17::INSTR'  ) # signal generator
-keithley2182A = rm.open_resource(  'GPIB0::8::INSTR'  ) # nanovoltmeter
-ADCMT6240a = rm.open_resource(  'GPIB0::11::INSTR'  ) # source voltage
+AgilentN5183A = rm.open_resource(  'GPIB0::19::INSTR'  ) # signal generator
+keithley2182A = rm.open_resource(  'GPIB0::6::INSTR'  ) # nanovoltmeter
+ADCMT6240a = rm.open_resource(  'GPIB0::1::INSTR'  ) # source voltage
 
 # initialize
-keithley2182A.write("*RST")
+#keithley2182A.write("*RST")
 AgilentN5183A.write('*RST')
 ADCMT6240a.write('C,*RST')
 
@@ -39,17 +39,37 @@ ADCMT6240a.write('F2')  # current measurement
 ADCMT6240a.write('SOV0,LMI0.03')  # dc 0V, limit 30mA
 ADCMT6240a.write('OPR') # output on
 
+def set_field(field):
+    V = float(ADCMT6240a.query('SOV?')[3:])
+    dV= abs(field-V)
+    i=0
+    while dV>0.2:
+        #V = float(ADCMT6240a.query('SOV?')[3:])
+        if (field-V)>0:
+            V +=0.2
+        else:
+            V -=0.2
+        ADCMT6240a.write('SOV%f' % V)
+        time.sleep(0.05)
+        V = float(ADCMT6240a.query('SOV?')[3:])
+        dV= abs(field-V)
+        i+=1
+    else:
+        ADCMT6240a.write('SOV%f' % field)
 
 def single_STFMR(P,f,datapoint, field_slope , field_offset, start=-7,end=7):
     AgilentN5183A.write(':POW %d DBM' %P)
     AgilentN5183A.write(':FREQ %d GHz' %f)
+    set_field(start)
+    time.sleep(0.5)
     mag = []
     Vmix =[]
-    for i in range(datapoint):
+    for i in range(datapoint+1):
         slope=(end-start)/datapoint
         V=slope*(i-datapoint//2)
         H=field_slope*V+field_offset
         ADCMT6240a.write('SOV%f' % V)
+        time.sleep(0.05)
         V_DC=float(keithley2182A.query(":SENSe:Data?")) # read voltage
         mag.append(H)
         Vmix.append(V_DC)
@@ -57,4 +77,12 @@ def single_STFMR(P,f,datapoint, field_slope , field_offset, start=-7,end=7):
 
 
 if __name__ == '__main__':
-    single_STFMR(P=20, f=6, datapoint=1400, field_slope=42.7, field_offset=0.1)
+    for i in np.arange(4,12,1):
+        H,V = single_STFMR(P=20, f=i, datapoint=1400, field_slope=42.8, field_offset=-0.1)
+        plt.plot(H,V,'-',label= str(i)+'GHz')
+        plt.xlabel('μ0H(mT)',fontsize=20)
+        plt.ylabel('V_DC(uV)',fontsize=20)
+        plt.legend()
+    plt.tight_layout()
+    plt.show()
+    #set_field(7)
